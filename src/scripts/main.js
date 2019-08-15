9ed28ffa19e819e074270f3e2239f00559afde30
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2019-08-15 14:41:02 -0500

    words

--- src/scripts/main.js
+++
@@ -1,5 +0,0 @@
-const message = "Time to build an application that gives you all the information you need in a Nutshell"
-
-document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
-
-console.log(message)
